1b80b33e85c1999f3f6d34085e8599392bc845b7
filipeapdo <filipeaparecido@gmail.com>
2024-04-07 07:14:04 -0300

    feat!: first release
    BREAKING CHANGE: simple flow to show, add and delete tasks

--- main.go
+++ main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"slices"
+	"strconv"
 )
 
+type Task struct {
+	ID    int
+	Title string
+	Done  bool
+}
+
+var tasks = []Task{
+	{1, "bla", false},
+	{2, "ble", true},
+}
+
+func remove(slice []Task, s int) []Task {
+	idx := slices.IndexFunc(slice, func(t Task) bool { return t.ID == s })
+	log.Println(idx)
+	return append(slice[:idx], slice[idx+1:]...)
+}
+
 func main() {
-	fmt.Println("FILIPUS To Do App")
+	log.Println("FILIPUS To Do App")
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl, err := template.ParseFiles("index.html")
 		if err != nil {
@@ -16,6 +35,35 @@ func main() {
 			return
 		}
 		tmpl.Execute(w, nil)
+	})
+
+	http.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
+		tmpl, err := template.ParseFiles("tasks.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		tmpl.Execute(w, tasks)
+	})
+
+	http.HandleFunc("/add-task", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" {
+			title := r.FormValue("title")
+			tasks = append(tasks, Task{len(tasks) + 1, title, false})
+			log.Println(tasks)
+			http.Redirect(w, r, "/tasks", http.StatusFound)
+		}
+	})
+
+	http.HandleFunc("/delete-task", func(w http.ResponseWriter, r *http.Request) {
+		id, err := strconv.Atoi(r.FormValue("id"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		tasks = remove(tasks, id)
+		log.Println(tasks)
+		http.Redirect(w, r, "/tasks", http.StatusFound)
 	})
 
 	log.Println("Starting server on :8888")
