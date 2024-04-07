953b0539ec9ddf65204a231f7f2a2853568bfe2a
filipeapdo <filipeaparecido@gmail.com>
2024-04-07 11:04:13 -0300

    fix: change templating from file to inline html

--- api/tasks.go
+++ api/tasks.go
@@ -16,8 +16,22 @@ var tasks = []Task{
 	{2, "ble", true},
 }
 
+const tmplTasks = `
+<div>
+  <span>{{.ID}} - {{.Title}}</span>
+  <form
+    hx-post="/delete-task"
+    hx-target="#todo-list"
+    hx-confirm="Deleting a task?"
+  >
+    <input type="hidden" name="id" value="{{.ID}}" />
+    <button type="submit">Delete</button>
+  </form>
+</div>
+`
+
 func Tasks(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("tasks_view.html")
+	tmpl, err := template.New("tasks").Parse(tmplTasks)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
